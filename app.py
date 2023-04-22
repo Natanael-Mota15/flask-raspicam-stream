251be7d04bfa9ddf4abb63df733e907243b7e416
Natanael-Mota15 <natanael.mota@csedge.org>
2023-04-22 15:52:24 -0400

    failed code

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template, Response
 from rpi_camera import RPiCamera
+from imageai.Detection import ObjectDetection
+import cv2
+
+cam_feed=cv2.VideoCapture(0)
+obj_detect = ObjectDetection()
+obj_detect.setModelTypeAsYOLOv3
+obj_detect.setModelPath(r"C:/Datasets/yolo.h5")
+
+
+
 
 app = Flask(__name__)
 
@@ -12,20 +22,32 @@ def index():
 def gen(camera):
 
     while True:
-        frame = camera.get_frame()
+        ret, img = cam_feed.read()   
+        annotated_image, preds = obj_detect.detectObjectsFromImage(input_image=img,
+                    input_type="array",
+                      output_type="array",
+                      display_percentage_probability=False,
+                      display_object_name=True)
+
+        cv2.imshow("", annotated_image)     
+    
+        if (cv2.waitKey(1) & 0xFF == ord("q")) or (cv2.waitKey(1)==27):
+            break
+
 
         # Each frame is set as a jpg content type. Frame data is in bytes.
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+        
 
 @app.route('/stream')
 def stream():
-
+    
     feed = Response(gen(RPiCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
     print(type(feed))
     return feed
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, threaded=True )
